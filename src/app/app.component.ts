e46659a8310498a0e61d8bdddf95504b6eb8a287
LeoDumas <leo.ndumas980@gmail.com>
2024-07-09 16:47:34 +0200

    Base app for todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,56 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todo-angular';
+
+  newTodo: string = '';
+  Todos: string[] = [];
+
+  ngOnInit() {
+    this.loadTodos();
+  }
+
+  addTodo() {
+    if (this.newTodo.trim() === '') {
+      alert('Vous devez ajouter un titre');
+    } else {
+      this.Todos.push(this.newTodo);
+      this.newTodo = '';
+      this.saveTodos();
+    }
+  }
+
+  removeTodo(index: number) {
+    this.Todos.splice(index, 1);
+    this.saveTodos();
+  }
+
+  modifyTodo(index: number) {
+    const newTitle = prompt('Modifier la tâche:', this.Todos[index]);
+    if (newTitle !== null) {
+      this.Todos[index] = newTitle;
+    }
+    this.saveTodos();
+  }
+
+  saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(this.Todos));
+  }
+
+  loadTodos() {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      this.Todos = JSON.parse(storedTodos);
+    }
+  }
 }
